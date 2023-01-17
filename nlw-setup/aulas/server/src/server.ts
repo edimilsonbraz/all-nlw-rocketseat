fa0882d012fdc7f01ce9812f0e4b04871728b1ec
edimilson braz <edimilson.gt8@gmail.com>
2023-01-17 13:06:26 -0300

    :sparkles: First Commit /start Setup

---
+++ nlw-setup/aulas/server/src/server.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+import fastify from "fastify";
+
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/hello', async () => {
+  const habits = await prisma.habit.findMany({
+    where: {
+      title: {
+        startsWith: 'Beber'
+      }
+    }
+  })
+
+  return habits
+})
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log('=> HTTP Server running')
+})
