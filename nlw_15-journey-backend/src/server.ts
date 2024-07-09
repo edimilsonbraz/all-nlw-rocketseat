8111815a15ddf66bdb1c21c83c8972dbd98afcbb
edimilsonbraz <edimilson.gt8@gmail.com>
2024-07-09 09:04:02 -0300

    :wrench: add setup prisma

--- nlw_15-journey-backend/src/server.ts
+++ nlw_15-journey-backend/src/server.ts
@@ -1,11 +1,26 @@
-import fastify from "fastify";
+import fastify from "fastify"
+import { prisma } from "./lib/prisma"
 
 const app = fastify()
 
-app.get('/', () => {
-  return 'Hello world'
+app.get("/cadastrar", async () => {
+  await prisma.trip.create({
+    data: {
+      destination: 'Brasilia',
+      starts_at: new Date(),
+      ends_at: new Date(),
+    }
+  })
+
+  return 'Registro cadastrado com sucesso!'
+})
+
+app.get('/listar', async () => {
+  const trips = await prisma.trip.findMany()
+
+  return trips
 })
 
 app.listen({ port: 3333 }).then(() => {
-  console.log('Server running')
+  console.log("Server running")
 })
