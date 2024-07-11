89729af5e501193d74bc530241af3c95633bd43f
edimilsonbraz <edimilson.gt8@gmail.com>
2024-07-11 13:47:55 -0300

    :sparkles: add confirmation route participants

--- nlw_15-journey-backend/src/routes/confirm-trip.ts
+++ nlw_15-journey-backend/src/routes/confirm-trip.ts
@@ -1,10 +1,10 @@
 import { FastifyInstance } from "fastify"
 import { ZodTypeProvider } from "fastify-type-provider-zod"
-import { z } from "zod"
-import { prisma } from "../lib/prisma"
-import { getMailClient } from "../lib/mail"
-import { dayjs } from "../lib/dayjs"
 import nodemailer from "nodemailer"
+import { z } from "zod"
+import { dayjs } from "../lib/dayjs"
+import { getMailClient } from "../lib/mail"
+import { prisma } from "../lib/prisma"
 
 export async function confirmTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get(
@@ -52,7 +52,7 @@ export async function confirmTrip(app: FastifyInstance) {
 
       await Promise.all(
         trip.participant.map(async (item) => {
-          const confirmationLink = `http://localhost:3333/trips/${trip.id}/${item.id}`
+          const confirmationLink = `http://localhost:3333/participants/${item.id}/confirm`
 
           const message = await mail.sendMail({
             from: {
