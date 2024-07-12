4efda4bd29f9342b0a3e36020352485edb6b6241
edimilsonbraz <edimilson.gt8@gmail.com>
2024-07-12 09:25:28 -0300

    :sparkles: add get trip-details route

--- nlw_15-journey-backend/src/server.ts
+++ nlw_15-journey-backend/src/server.ts
@@ -7,7 +7,11 @@ import { confirmParticipants } from "./routes/confirm-participant";
 import { createActivity } from "./routes/create-activity";
 import { getActivities } from "./routes/get-activities";
 import { createLink } from "./routes/create-link";
-import { getLinks } from "./routes/get-links";
+import { getParticipants } from "./routes/get-participants";
+import { createInvite } from "./routes/create-invite";
+import { updateTrip } from "./routes/update-trip";
+import { getTripDetails } from "./routes/get-trip-details";
+import { getParticipant } from "./routes/get-participant";
 
 const app = fastify()
 
@@ -24,7 +28,11 @@ app.register(confirmParticipants)
 app.register(createActivity)
 app.register(getActivities)
 app.register(createLink)
-app.register(getLinks)
+app.register(getParticipants)
+app.register(createInvite)
+app.register(updateTrip)
+app.register(getTripDetails)
+app.register(getParticipant)
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server running")
