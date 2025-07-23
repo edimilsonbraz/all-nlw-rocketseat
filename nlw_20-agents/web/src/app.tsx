1ee2a2958c23a50925ac1c3d66bfb8adfc8ee643
edimilsonbraz <edimilson.gt8@gmail.com>
2025-07-23 15:19:13 -0300

    :sparkles: create record page

--- nlw_20-agents/web/src/app.tsx
+++ nlw_20-agents/web/src/app.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { CreateRoom } from "./pages/create-room"
 import { Room } from "./pages/room"
+import { RecordRoomAudio } from "./pages/record-room-audio"
 
 // Pra fazer chamadas HTTP
 const queryClient = new QueryClient()
@@ -13,6 +14,7 @@ export function App() {
         <Routes>
           <Route element={<CreateRoom />} index />
           <Route element={<Room />} path="/room/:roomId" />
+          <Route element={<RecordRoomAudio />} path="/room/:roomId/audio" />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
