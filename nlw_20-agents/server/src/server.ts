ad880443c1271cad971546fa230d62db52240170
edimilsonbraz <edimilson.gt8@gmail.com>
2025-07-24 16:06:28 -0300

    :sparkles: upload routes

--- nlw_20-agents/server/src/server.ts
+++ nlw_20-agents/server/src/server.ts
@@ -1,5 +1,6 @@
 import "dotenv/config"
 import { fastifyCors } from "@fastify/cors"
+import { fastifyMultipart } from "@fastify/multipart"
 import { fastify } from "fastify"
 import {
   serializerCompiler,
@@ -11,12 +12,15 @@ import { getRoomsRoute } from "./http/routes/get-rooms.ts"
 import { createRoomRoute } from "./http/routes/create-room.ts"
 import { getRoomsQuestions } from "./http/routes/get-room-questions.ts"
 import { createQuestionRoute } from "./http/routes/create-question.ts"
+import { uploadAudioRoute } from "./http/routes/upload-audio.ts"
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
   origin: "http://localhost:5173"
 })
+
+app.register(fastifyMultipart)
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
@@ -29,5 +33,6 @@ app.register(getRoomsRoute)
 app.register(createRoomRoute)
 app.register(getRoomsQuestions)
 app.register(createQuestionRoute)
+app.register(uploadAudioRoute)
 
 app.listen({ port: env.PORT })
